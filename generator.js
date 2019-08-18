222c1f6c35170129f4dff141481dcbdb14e97c50
Brooke Boatman <bboatman1241@gmail.com>
2019-08-17 21:31:29 -0400

    simplified generator logic and added random length to name feature

--- generator.js
+++ generator.js
@@ -1,18 +1,36 @@
 // This is the main code to generate the character name //
 
+// List of consonants
+var con = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+// List of vowels
+var vow = ["a", "e", "i", "o", "u"];
+
+// Generate a name based on format and desired length;
+function generateName(format, length){
+	ret = "";
+	switch (format) {
+		case "alternating":
+			for (let i = 0; i < length; i++){
+				let src = i % 2 == 0 ? con : vow;
+				let ind = Math.floor(Math.random() * src.length);
+				ret += src[ind];
+			}
+			break;
+	
+		default:
+			for (let i = 0; i < length; i++){
+				let src = i % 3 == 1 ? vow : con;
+				let ind = Math.floor(Math.random() * src.length);
+				ret += src[ind];
+			}
+			break;
+	}
+	return ret;
+}
+
 function generator(){
-
-	// List of consonants
-	var con = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-
-	// List of vowels
-	var vow = ["a", "e", "i", "o", "u"]
-
-	// Generates a name in CVCCVC format
-	var name1 = con[Math.floor(Math.random() * con.length)] + vow[Math.floor(Math.random() * vow.length)] + con[Math.floor(Math.random() * con.length)] + con[Math.floor(Math.random() * con.length)] + vow[Math.floor(Math.random() * vow.length)] + con[Math.floor(Math.random() * con.length)];
-
-	// Generates a name in CVCVCV format
-	var name2 = con[Math.floor(Math.random() * con.length)] + vow[Math.floor(Math.random() * vow.length)] + con[Math.floor(Math.random() * con.length)] + vow[Math.floor(Math.random() * vow.length)] + con[Math.floor(Math.random() * con.length)] + vow[Math.floor(Math.random() * vow.length)];
+	//Generate random length for name between 4 and 7
+	let len = 4 + Math.floor(Math.random() * Math.floor(4));
 
 	// Clears any previous generated name
 	if (document.getElementById("name")) {
@@ -26,13 +44,12 @@ function generator(){
 	// Shows name in format CVCCVC
 	var element = document.createElement("div");
 	element.setAttribute("id", "name");
-	element.appendChild(document.createTextNode(name1));
+	element.appendChild(document.createTextNode(generateName('alternating', len)));
 	document.getElementById("placeholder1").appendChild(element);
 
 	// Shows name in format CVCVCV
 	var element = document.createElement("div");
 	element.setAttribute("id", "name");
-	element.appendChild(document.createTextNode(name2));
+	element.appendChild(document.createTextNode(generateName('default', 6)));
 	document.getElementById("placeholder2").appendChild(element);
-
 }
